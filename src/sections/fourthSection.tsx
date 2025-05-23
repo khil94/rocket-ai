16a268604e9d055b17e5dde7276895c416dc2951
khil94 <zidru94@gmail.com>
2025-05-23 18:30:18 +0900

    chore : 4th section 이름과 출생일이 변수를 참조하게 수정
    - 별건아니고, 형식적으로 모양변경

--- src/sections/fourthSection.tsx
+++ src/sections/fourthSection.tsx
@@ -3,6 +3,8 @@ import { useResponsiveRate } from "@/hooks/useResponsiveRate";
 
 export default function FourthSection() {
   const rate = useResponsiveRate();
+  const name = "김로켓";
+  const birth = "1980년 8월27일 08:10";
   return (
     <div
       className={`relative -mt-[5%] w-full h-full flex justify-center
@@ -14,10 +16,8 @@ export default function FourthSection() {
         style={{ width: `${rate * 351}px`, height: `${621 * rate}px` }}
       >
         <div className="flex flex-col text-center gap-[.5vh]">
-          <span className="ssm:text-xl text-[4vw]">김로켓님의 사주</span>
-          <span className="ssm:text-2xl text-[5vw] font-bold">
-            1980년 8월27일 08:10
-          </span>
+          <span className="ssm:text-xl text-[4vw]">{name}님의 사주</span>
+          <span className="ssm:text-2xl text-[5vw] font-bold">{birth}</span>
         </div>
 
         <SazuTable />
