5663e2e1a1b810eccc0ea14c4c6a6042a3608235
khil94 <zidru94@gmail.com>
2025-05-23 18:17:35 +0900

    style : chapter에 text 색 명시

--- src/components/chapter.tsx
+++ src/components/chapter.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function Chapter({ upperText, lowerText }: Props) {
   const rate = useResponsiveRate();
   return (
-    <div className="absolute text-center flex flex-col gap-2 top-1/6 left-1/2 -translate-x-1/2">
+    <div className="absolute text-white text-center flex flex-col gap-2 top-1/6 left-1/2 -translate-x-1/2">
       <span className="animate-fade-up ">{upperText}</span>
       <Mark width={157 * rate} />
       <span className="animate-fade-up ">{lowerText}</span>
