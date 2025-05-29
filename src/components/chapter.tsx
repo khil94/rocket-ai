055adb7efa79133149d7e56dd6827c0cfa40d70f
khil94 <zidru94@gmail.com>
2025-05-29 12:44:08 +0900

    style : chapter 글자에도 마저 적용
    - 사이즈에 따른 글자 스케일링이 미적용 되어 수정

--- src/components/chapter.tsx
+++ src/components/chapter.tsx
@@ -10,9 +10,13 @@ export default function Chapter({ upperText, lowerText }: Props) {
   const rate = useResponsiveRate();
   return (
     <div className="absolute text-white text-center flex flex-col gap-2 top-1/6 left-1/2 -translate-x-1/2">
-      <span className="animate-fade-up ">{upperText}</span>
+      <span className="animate-fade-up ssm:text-base text-[4vw]">
+        {upperText}
+      </span>
       <Mark width={157 * rate} />
-      <span className="animate-fade-up ">{lowerText}</span>
+      <span className="animate-fade-up ssm:text-base text-[4vw]">
+        {lowerText}
+      </span>
     </div>
   );
 }
